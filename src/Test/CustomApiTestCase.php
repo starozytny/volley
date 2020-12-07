caaf9bd116206b04dc0e9c3aef11edc578db0fd3
Chanbora <chanbora@logilink.fr>
2020-12-07 17:22:38 +0100

    init test case

---
+++ src/Test/CustomApiTestCase.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Test;
+
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+
+class CustomApiTestCase extends ApiTestCase
+{
+    public function createUser()
+    {
+        
+    }
+
+    public function logIn($client, $username, $password)
+    {
+        
+    }
+}
--- tests/Functional/UserTest.php
+++ tests/Functional/UserTest.php
@@ -4,26 +4,19 @@
 namespace App\Tests\Functional;
 
 
-use App\Repository\UserRepository;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
 
-class UserTest extends WebTestCase
+class UserTest extends ApiTestCase
 {
     public function testLogin()
     {
         $client = static::createClient();
+
+
+
         $client->request('GET', '/api/account', [
-            'HTTP_AUTHORIZATION ' => "Bearer 55e21a3efcc855ae0a45c5015c17641ffdc14ff1b07d157fc6c60c5566d0ad095192437d9906e19849cb764bd73abf66ff76b355408e5ef9fa66d55e"
+            'auth_bearer' => "55e21a3efcc855ae0a45c5015c17641ffdc14ff1b07d157fc6c60c5566d0ad095192437d9906e19849cb764bd73abf66ff76b355408e5ef9fa66d55e"
         ]);
-        $this->assertResponseIsSuccessful();
-
-        $userRepository = static::$container->get(UserRepository::class);
-
-        $testUser = $userRepository->findOneByUsername('shanbo');
-
-        $client->loginUser($testUser);
-
-        $client->request('GET', '/api/account');
         $this->assertResponseIsSuccessful();
     }
 }
