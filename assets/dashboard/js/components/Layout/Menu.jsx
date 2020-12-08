238624e70a5ae32f3465e4d9a6b02d7ec843609d
Chanbora <chanbora@logilink.fr>
2020-12-08 15:03:17 +0100

    menu mobile

--- assets/dashboard/js/components/Layout/Menu.jsx
+++ assets/dashboard/js/components/Layout/Menu.jsx
@@ -3,27 +3,51 @@ import React, { Component } from 'react';
 export class Menu extends Component {
     constructor(props) {
         super();
+
+        this.state = {
+            isOpened: false
+        }
+
+        this.handleOpenMenu = this.handleOpenMenu.bind();
+    }
+
+    handleOpenMenu = () => {
+        this.setState(prevState => {
+            return { isOpened: !prevState.isOpened }
+        })
     }
 
     render () {
-        const { menu } = this.props
+        const { menu, menuBottom } = this.props
+        const { isOpened } = this.state
 
         return <>
-            <div className="nav-mobile">
-                <span className="icon-menu"/>
+            <div className="nav-mobile" onClick={this.handleOpenMenu}>
+                <span className={`icon-${ isOpened ? "cancel" : "menu" }`} />
             </div>
-            <div className="nav-body">
-                <div className="menu">
-                    {JSON.parse(menu).map(el => {
-                        return <div key={el.name} className="item">
-                            <a href={el.path}>
-                                <span className={`icon-${el.icon}`} />
-                                <span>{el.label}</span>
-                            </a>
-                        </div>
-                    })}
+            <div className={`nav-body ${isOpened}`}>
+                <div className="items">
+                    { <MenuItem menu={menu} /> }
+                </div>
+                <div className="items">
+                    { <MenuItem menu={menuBottom} /> }
                 </div>
             </div>
         </>
     }
+}
+
+function MenuItem (props){
+    const { menu } = props
+
+    return (
+        JSON.parse(menu).map(el => {
+            return <div key={el.name} className="item">
+                <a href={el.path}>
+                    <span className={`icon-${el.icon}`} />
+                    <span>{el.label}</span>
+                </a>
+            </div>
+        })
+    )
 }
