b5ddbb1e41ad1f19bbb9f935e01eacf002018155
Chanbora <chanbora@logilink.fr>
2020-12-10 11:57:58 +0100

    style liste items table

--- assets/dashboard/js/pages/components/User/UserItems.jsx
+++ assets/dashboard/js/pages/components/User/UserItems.jsx
@@ -6,15 +6,29 @@ export function UserItems (props) {
     return <div className="items items-user">
         {data.map(elem => {
             return <div className="item" key={elem.id}>
+
                 <div className="item-selector" />
+
                 <div className="item-content">
-                    <div className="avatar">
-                        <img src={`https://robohash.org/${elem.username}?size=64x64`} alt={`Avatar de ${elem.username}`}/>
+
+                    <div className="item-body">
+                        <div className="avatar">
+                            <img src={`https://robohash.org/${elem.username}?size=64x64`} alt={`Avatar de ${elem.username}`}/>
+                        </div>
+                        <div className="infos">
+                            <div>
+                                <div className="username">{elem.username}</div>
+                                <div className="email">{elem.email}</div>
+                            </div>
+                            <div className="actions">
+                                <div className="btn-icon">
+                                    <span className="icon-pencil" />
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
-                    <div className="infos">
-                        <div className="username">{elem.username}</div>
-                        <div className="email">{elem.email}</div>
-                    </div>
+
                 </div>
             </div>
         })}
