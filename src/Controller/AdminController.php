994d27faeb479373667c13ba6d2673481b1a53c1
Chanbora <chanbora@logilink.fr>
2020-12-23 14:46:30 +0100

    remove useless controller

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,15 +6,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/admin", name="admin_")
+ */
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin_homepage")
+     * @Route("/", name="homepage")
      */
     public function index(): Response
     {
         return $this->render('admin/pages/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
+    }
+
+    /**
+     * @Route("/styleguide", name="styleguide")
+     */
+    public function styleguide(): Response
+    {
+        return $this->render('admin/pages/styleguide/index.html.twig');
+    }
+
+    /**
+     * @Route("/utilisateurs", name="users_index")
+     */
+    public function users(): Response
+    {
+        return $this->render('admin/pages/user/index.html.twig');
     }
 }
