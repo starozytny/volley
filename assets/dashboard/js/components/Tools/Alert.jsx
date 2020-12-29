585cb289c439f9a2167d443d387e769b89c4b5cd
Chanbora <chanbora@logilink.fr>
2020-12-29 11:42:21 +0100

    alert with custom content

--- assets/dashboard/js/components/Tools/Alert.jsx
+++ assets/dashboard/js/components/Tools/Alert.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export function Alert(props){
-    const { type, title, withIcon = true, children } = props;
+    const { type, title, withIcon = true, content=null, children } = props;
 
     let icon, alert;
     switch (type){
@@ -25,9 +25,10 @@ export function Alert(props){
 
     return <div className={`alert alert-${alert}`}>
         {withIcon && <span className={`icon-${icon}`} />}
-        <p>
+        {content}
+        {children && <p>
             {title && <span className="title">{title}</span>}
             {children}
-        </p>
+        </p>}
     </div>
 }
