971c62e229c140bd8245cc7601d6469536e72d97
Chanbora <chanbora@logilink.fr>
2021-01-20 15:28:56 +0100

    add legales pages

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -15,4 +15,36 @@ class AppController extends AbstractController
     {
         return $this->render('app/pages/index.html.twig');
     }
+
+    /**
+     * @Route("/legales/mentions-legales", name="app_mentions")
+     */
+    public function mentions(): Response
+    {
+        return $this->render('app/pages/legales/mentions.html.twig');
+    }
+
+    /**
+     * @Route("/legales/politique-confidentialite", name="app_politique")
+     */
+    public function politique(): Response
+    {
+        return $this->render('app/pages/legales/politique.html.twig');
+    }
+
+    /**
+     * @Route("/legales/cookies", name="app_cookies")
+     */
+    public function cookies(): Response
+    {
+        return $this->render('app/pages/legales/cookies.html.twig');
+    }
+
+    /**
+     * @Route("/legales/rgpd", name="app_rgpd")
+     */
+    public function rgpd(): Response
+    {
+        return $this->render('app/pages/legales/rgpd.html.twig');
+    }
 }
