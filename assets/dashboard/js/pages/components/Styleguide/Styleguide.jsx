7f78a9005ff3aad5d03481c8e682d4524165add7
Shanbora <chanbora@logilink.fr>
2021-04-07 10:23:47 +0200

    init maps

--- assets/dashboard/js/pages/components/Styleguide/Styleguide.jsx
+++ assets/dashboard/js/pages/components/Styleguide/Styleguide.jsx
@@ -3,11 +3,13 @@ import React, { Component } from "react";
 import { StyleguideButton }   from "./components/StyleguideButton";
 import { StyleguideAlert }    from "./components/StyleguideAlert";
 import { StyleguideForm }     from "./components/StyleguideForm";
-import { StyleguideAside }     from "./components/StyleguideAside";
+import { StyleguideAside }    from "./components/StyleguideAside";
+import { StyleguideMaps }     from "./components/StyleguideMaps";
 
 export class Styleguide extends Component {
     render () {
         return <div className="main-content">
+            <StyleguideMaps />
             <StyleguideAside />
             <StyleguideForm />
             <StyleguideButton />
---
+++ assets/dashboard/js/pages/components/Styleguide/components/StyleguideMaps.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import {Alert} from "@dashboardComponents/Tools/Alert";
+
+export function StyleguideMaps () {
+    return (
+        <section>
+            <h2>OpenstreetMaps - Leaflet</h2>
+            <div className="maps-items">
+            </div>
+        </section>
+    )
+}
