875787737cd34ce2f3bcb0f18e68f0c0c658560d
Shanbora <chanbora@logilink.fr>
2021-04-07 14:12:05 +0200

    add custom div icon

--- assets/dashboard/js/pages/components/Styleguide/components/StyleguideMaps.jsx
+++ assets/dashboard/js/pages/components/Styleguide/components/StyleguideMaps.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 
+import axios from "axios";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet/dist/leaflet";
+import "leaflet-ajax/dist/leaflet.ajax.min";
 
 export class StyleguideMaps extends Component{
     componentDidMount = () => {
@@ -11,17 +13,25 @@ export class StyleguideMaps extends Component{
             maxZoom: 18,
         }).addTo(mymap);
 
-        let leafletIcon = L.icon({
-            iconUrl: '../../maps/images/marker-icon.png',
-            shadowUrl: '../../maps/images/marker-shadow.png',
-            iconSize:     [25, 41], // size of the icon
-            shadowSize:   [41, 41], // size of the shadow
-            iconAnchor:   [9, 40], // point of the icon which will correspond to marker's location 38 95
-            shadowAnchor: [9, 40],  // the same for the shadow 50 61
-            popupAnchor:  [4, -35] // point from which the popup should open relative to the iconAnchor
+        // let leafletIcon = L.icon({
+        //     iconUrl: '../../maps/images/marker-icon.png',
+        //     shadowUrl: '../../maps/images/marker-shadow.png',
+        //     iconSize:     [25, 41], // size of the icon
+        //     shadowSize:   [41, 41], // size of the shadow
+        //     iconAnchor:   [9, 40], // point of the icon which will correspond to marker's location 38 95
+        //     shadowAnchor: [9, 40],  // the same for the shadow 50 61
+        //     popupAnchor:  [4, -35] // point from which the popup should open relative to the iconAnchor
+        // })
+        let leafletIcon = L.divIcon({
+            className: 'custom-div-icon',
+            html: "<div style='background-color:#4838cc;' class='marker-pin'></div><i class='icon-book'>",
+            iconSize: [30, 42],
+            iconAnchor: [15, 42],
+            popupAnchor:  [0, -35]
         })
 
         let marker = L.marker([51.5, -0.09], {icon: leafletIcon}).addTo(mymap);
+        marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
         let circle = L.circle([51.508, -0.11], {
             color: 'red',
             fillColor: '#f03',
@@ -34,7 +44,32 @@ export class StyleguideMaps extends Component{
             [51.51, -0.047]
         ]).addTo(mymap);
 
-        marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+        let geojsonFeature = {
+            "type": "Feature",
+            "properties": {
+                "name": "Coors Field",
+                "amenity": "Baseball Stadium",
+                "popupContent": "This is where the Rockies play!"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates":[-0.09, 51.495]
+            }
+        };
+        L.geoJSON(geojsonFeature, {pointToLayer: function (feature, latlng) {
+                return L.marker(latlng, {icon: leafletIcon});
+            },}).addTo(mymap);
+
+        axios.get('../../maps/data/ecoles.json')
+            .then(function (response){
+                response.data.forEach(el => {
+                    let marker = L.marker([el.Latitude, el.Longitude], {icon: leafletIcon}).addTo(mymap);
+                    marker.bindPopup("<b>"+el.name+"</b> <br/>" + el.Categorie).openPopup();
+                })
+                console.log(response.data)
+            })
+        ;
+
     }
 
     render () {
