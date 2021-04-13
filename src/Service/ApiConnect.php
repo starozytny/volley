ee23be471318510647b334a400dd6a334c09f34a
Shanbora <chanbora@logilink.fr>
2021-04-13 11:08:57 +0200

    global apiConnect

--- src/Service/ApiConnect.php
+++ src/Service/ApiConnect.php
@@ -4,33 +4,41 @@
 namespace App\Service;
 
 
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ApiConnect
 {
-    private $maps_api; //scope client
+    private $api_immo; //scope client
 
-    public function __construct(HttpClientInterface $maps_api)
+    public function __construct(HttpClientInterface $api_immo)
     {
-        $this->maps_api = $maps_api;
+        $this->api_immo = $api_immo;
     }
 
-    public function connect()
+    /**
+     * @throws RedirectionExceptionInterface
+     * @throws ClientExceptionInterface
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     */
+    public function connect($method, $url): string
     {
-        $response = $this->maps_api->request(
-            'GET',
-            'https://api.demodirecte.fr/api/maps/data'
-        );
+        $response = $this->api_immo->request($method, $url);
 
         $statusCode = $response->getStatusCode();
         // $statusCode = 200
         $contentType = $response->getHeaders()['content-type'][0];
         // $contentType = 'application/json'
-        $content = $response->getContent();
+
+        // $response->getContent();
         // $content = '{"id":521583, "name":"symfony-docs", ...}'
-//        $content = $response->toArray();
+        //  $content = $response->toArray();
         // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
 
-        return $content;
+        return $response->getContent();
     }
 }
