34c9e7863389c000a9866e089ed6a340e7325330
Shanbora <chanbora@logilink.fr>
2021-06-01 14:35:58 +0200

    add menu dropdown

--- assets/app/js/app.js
+++ assets/app/js/app.js
@@ -8,15 +8,3 @@ const menu = document.getElementById("menu");
 render(
     <Menu {...menu.dataset} />, menu
 )
-
-// let menu = document.querySelector(".nav-mobile");
-// if(menu){
-//     let menuBody = document.querySelector(".nav-body");
-//     menu.addEventListener('click', function (){
-//         if(menuBody.classList.contains('true')){
-//             menuBody.classList.remove('true');
-//         }else{
-//             menuBody.classList.add('true');
-//         }
-//     })
-// }
