6416705f6931430bc260730a42485f0acedeaee4
Shanbora <chanbora@logilink.fr>
2021-06-02 14:22:05 +0200

    facto page layout

--- assets/dashboard/js/components/Layout/Page.jsx
+++ assets/dashboard/js/components/Layout/Page.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 
-import { PageError } from "./PageError";
-import { Pagination } from "./Pagination";
+import { PageError }        from "./PageError";
+import { Pagination }       from "./Pagination";
+import { LoaderElement }    from "@dashboardComponents/Layout/Loader";
+
+import Sort                 from "@dashboardComponents/functions/sort";
+import Formulaire           from "@dashboardComponents/functions/Formulaire";
 
 export class Page extends Component {
     constructor(props) {
@@ -25,6 +29,87 @@ export class Page extends Component {
                             onUpdate={(items) => this.props.onUpdate(items)}/>
             </div>
 
+        </>
+    }
+}
+
+export class Layout extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            context: "list",
+            loadPageError: false,
+            loadData: true,
+            data: null,
+            currentData: null,
+            element: null,
+            perPage: props.perPage
+        }
+
+        this.page = React.createRef();
+
+        this.handleUpdateData = this.handleUpdateData.bind(this);
+        this.handleChangeContext = this.handleChangeContext.bind(this);
+        this.handleUpdateList = this.handleUpdateList.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+    }
+
+    componentDidMount() { this.props.onGetData(this); }
+
+    handleUpdateData = (data) => { this.setState({ currentData: data })  }
+
+    handleChangeContext = (context, element=null) => {
+        this.setState({ context, element });
+        if(context === "list"){
+            this.page.current.pagination.current.handleComeback()
+        }
+    }
+
+    handleUpdateList = (element, newContext=null) => {
+        const { data, context, perPage } = this.state
+        Formulaire.updateDataPagination(this, Sort.compareCreatedAt, newContext, context, data, element, perPage);
+    }
+
+    handleSearch = (search, searchFunction) => {
+        const { dataImmuable, perPage } = this.state;
+
+        if(search === "") {
+            this.setState({ data: dataImmuable, currentData: dataImmuable.slice(0, perPage) });
+        }else{
+            let newData = searchFunction(dataImmuable, search);
+            this.setState({ data: newData, currentData: newData.slice(0, perPage) });
+        }
+    }
+
+    render () {
+        const { onContentList, onContentCreate, onContentUpdate } = this.props;
+        const { loadPageError, context, loadData, data, currentData, element } = this.state;
+
+        let content, havePagination = false;
+        switch (context){
+            case "create":
+                content = onContentCreate(this.handleChangeContext, this.handleUpdateList)
+                break;
+            case "update":
+                content = onContentUpdate(this.handleChangeContext, this.handleUpdateList, element)
+                break;
+            default:
+                havePagination = true;
+                content = loadData ? <LoaderElement /> : onContentList(currentData, this.handleChangeContext)
+                break;
+        }
+
+        if(data && data.length <= 0){
+            havePagination = false;
+        }
+
+        return <>
+            <Page ref={this.page} haveLoadPageError={loadPageError}
+                  havePagination={havePagination} taille={data && data.length} data={data} onUpdate={this.handleUpdateData}
+            >
+                {content}
+            </Page>
         </>
     }
 }
--- assets/dashboard/js/pages/components/Blog/Articles.jsx
+++ assets/dashboard/js/pages/components/Blog/Articles.jsx
@@ -4,8 +4,7 @@ import axios             from "axios";
 import toastr            from "toastr";
 import Routing           from '@publicFolder/bundles/fosjsrouting/js/router.min.js';
 
-import { Page }          from "@dashboardComponents/Layout/Page";
-import { LoaderElement } from "@dashboardComponents/Layout/Loader";
+import {Layout }         from "@dashboardComponents/Layout/Page";
 import Sort              from "@dashboardComponents/functions/sort";
 import Formulaire        from "@dashboardComponents/functions/Formulaire";
 
@@ -13,35 +12,41 @@ import { ArticlesList }  from "./ArticlesList";
 import { ArticleCreate } from "./ArticleCreate";
 import { ArticleUpdate } from "./ArticleUpdate";
 
+function searchFunction(dataImmuable, search){
+    let newData = [];
+    newData = dataImmuable.filter(function(v) {
+        if(v.title.toLowerCase().includes(search)){
+            return v;
+        }
+    })
+
+    return newData;
+}
+
 export class Articles extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            context: "list",
-            loadPageError: false,
-            loadData: true,
-            data: null,
-            currentData: null,
-            element: null,
             perPage: 10
         }
 
-        this.page = React.createRef();
+        this.layout = React.createRef();
 
-        this.handleUpdateData = this.handleUpdateData.bind(this);
-        this.handleChangeContext = this.handleChangeContext.bind(this);
-        this.handleUpdateList = this.handleUpdateList.bind(this);
         this.handleDelete = this.handleDelete.bind(this);
         this.handleSearch = this.handleSearch.bind(this);
         this.handleDeleteGroup = this.handleDeleteGroup.bind(this);
         this.handleChangePublished = this.handleChangePublished.bind(this);
+
+        this.handleGetData = this.handleGetData.bind(this);
+        this.handleContentList = this.handleContentList.bind(this);
+        this.handleContentCreate = this.handleContentCreate.bind(this);
+        this.handleContentUpdate = this.handleContentUpdate.bind(this);
     }
 
-    componentDidMount() {
+    handleGetData = (self) => {
         const { perPage } = this.state;
 
-        const self = this;
         axios.get(Routing.generate('api_articles_index'), {})
             .then(function (response) {
                 let resp = response.data;
@@ -61,20 +66,6 @@ export class Articles extends Component {
         ;
     }
 
-    handleUpdateData = (data) => { this.setState({ currentData: data })  }
-
-    handleUpdateList = (element, newContext=null) => {
-        const { data, context, perPage } = this.state
-        Formulaire.updateDataPagination(this, Sort.compareCreatedAt, newContext, context, data, element, perPage);
-    }
-
-    handleChangeContext = (context, element=null) => {
-        this.setState({ context, element });
-        if(context === "list"){
-            this.page.current.pagination.current.handleComeback()
-        }
-    }
-
     handleDelete = (element) => {
         Formulaire.axiosDeleteElement(this, element, Routing.generate('api_articles_delete', {'id': element.id}),
             'Supprimer cet article ?', 'Cette action est irréversible.');
@@ -84,21 +75,7 @@ export class Articles extends Component {
         Formulaire.axiosDeleteGroupElement(this, checked, Routing.generate('api_articles_delete_group'), 'Aucun article sélectionné.')
     }
 
-    handleSearch = (search) => {
-        const { dataImmuable, perPage } = this.state;
-
-        if(search === "") {
-            this.setState({ data: dataImmuable, currentData: dataImmuable.slice(0, perPage) });
-        }else{
-            let newData = [];
-            newData = dataImmuable.filter(function(v) {
-                if(v.title.toLowerCase().includes(search)){
-                    return v;
-                }
-            })
-            this.setState({ data: newData, currentData: newData.slice(0, perPage) });
-        }
-    }
+    handleSearch = (search) => { this.layout.current.handleSearch(search, searchFunction) }
 
     handleChangePublished = (element) => {
         Formulaire.loader(true);
@@ -118,38 +95,28 @@ export class Articles extends Component {
         ;
     }
 
+    handleContentList = (currentData, changeContext) => {
+        return <ArticlesList onChangeContext={changeContext}
+                             onDelete={this.handleDelete}
+                             onSearch={this.handleSearch}
+                             onDeleteAll={this.handleDeleteGroup}
+                             onChangePublished={this.handleChangePublished}
+                             data={currentData} />
+    }
+
+    handleContentCreate = (changeContext, updateList) => {
+        return <ArticleCreate categories={this.layout.current.state.categories} onChangeContext={changeContext} onUpdateList={updateList}/>
+    }
+
+    handleContentUpdate = (changeContext, updateList, element) => {
+        return <ArticleUpdate categories={this.layout.current.state.categories} element={element} onChangeContext={changeContext} onUpdateList={updateList}/>
+    }
+
     render () {
-        const { loadPageError, context, loadData, data, currentData, element, categories } = this.state;
-
-        let content, havePagination = false;
-        switch (context){
-            case "create":
-                content = <ArticleCreate categories={categories} onChangeContext={this.handleChangeContext} onUpdateList={this.handleUpdateList} />
-                break;
-            case "update":
-                content = <ArticleUpdate categories={categories} onChangeContext={this.handleChangeContext} onUpdateList={this.handleUpdateList} element={element}/>
-                break;
-            default:
-                havePagination = true;
-                content = loadData ? <LoaderElement /> : <ArticlesList onChangeContext={this.handleChangeContext}
-                                                                   onDelete={this.handleDelete}
-                                                                   onSearch={this.handleSearch}
-                                                                   onDeleteAll={this.handleDeleteGroup}
-                                                                   onChangePublished={this.handleChangePublished}
-                                                                   data={currentData} />
-                break;
-        }
-
-        if(data && data.length <= 0){
-            havePagination = false;
-        }
-
         return <>
-            <Page ref={this.page} haveLoadPageError={loadPageError}
-                  havePagination={havePagination} taille={data && data.length} data={data} onUpdate={this.handleUpdateData}
-            >
-                {content}
-            </Page>
+            <Layout ref={this.layout} {...this.state} onGetData={this.handleGetData}
+                    onContentList={this.handleContentList}
+                    onContentCreate={this.handleContentCreate} onContentUpdate={this.handleContentUpdate}/>
         </>
     }
 }
