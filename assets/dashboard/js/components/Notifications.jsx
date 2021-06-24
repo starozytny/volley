ccdff852e8544cfe9b41819eff5555956a3fa729
Shanbora <chanbora@logilink.fr>
2021-06-24 11:34:07 +0200

    style general notif

--- assets/dashboard/js/components/Notifications.jsx
+++ assets/dashboard/js/components/Notifications.jsx
@@ -2,10 +2,40 @@ import React, { Component } from "react";
 
 export class Notifications extends Component{
     render() {
-        return <>
+        return <div className="notif-container">
             <div className="btn-notif">
                 <span className="icon-notification" />
+                <span className="number">5</span>
             </div>
-        </>
+            <div className="notif-card">
+                <div className="notif-cancel">
+                    <span>Notifications</span>
+                    <span className="icon-cancel" />
+                </div>
+                <div className="notif-body">
+                    <div className="item">
+                        <div className="item-icon">
+                            <span className="icon-user" />
+                        </div>
+                        <div className="item-infos">
+                            <div className="title">Nouveau utilisateur</div>
+                            <div className="createdAt">Il y a 5 minutes</div>
+                        </div>
+                    </div>
+                    <div className="item">
+                        <div className="item-icon">
+                            <span className="icon-user" />
+                        </div>
+                        <div className="item-infos">
+                            <div className="title">Nouveau utilisateur</div>
+                            <div className="createdAt">Il y a 5 minutes</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="notif-all">
+                    Voir toutes les notifications
+                </div>
+            </div>
+        </div>
     }
 }
