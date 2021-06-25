58809658863722c00ab0826feec47aa6ffa108ed
Shanbora <chanbora@logilink.fr>
2021-06-25 14:04:43 +0200

    style toastr

--- assets/app/js/app.js
+++ assets/app/js/app.js
@@ -4,11 +4,29 @@ const routes = require('@publicFolder/js/fos_js_routes.json');
 import Routing from '@publicFolder/bundles/fosjsrouting/js/router.min';
 
 import React from 'react';
+import toastr from 'toastr';
 import { render } from 'react-dom';
 
 import { Menu }     from "@dashboardComponents/Layout/Menu";
 import { Cookies, CookiesGlobalResponse } from "@appComponents/Tools/Cookies";
 
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": true,
+    "progressBar": true,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": true,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
 Routing.setRoutingData(routes);
 
 const menu = document.getElementById("menu");
--- assets/dashboard/js/app.js
+++ assets/dashboard/js/app.js
@@ -4,9 +4,28 @@ const routes = require('@publicFolder/js/fos_js_routes.json');
 import Routing from '@publicFolder/bundles/fosjsrouting/js/router.min';
 
 import React from 'react';
+import toastr from 'toastr';
 import { render } from 'react-dom';
 import { Menu } from './components/Layout/Menu';
-import {Notifications} from "@dashboardComponents/Notifications";
+import { Notifications } from "@dashboardComponents/Notifications";
+
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": true,
+    "progressBar": true,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": true,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
 
 Routing.setRoutingData(routes);
 
