4e1a049d84afea5265565673e4bb42d208837bc4
Shanbora <chanbora@logilink.fr>
2021-06-28 10:12:14 +0200

    table with col styleguide

--- assets/dashboard/js/pages/components/Styleguide/Styleguide.jsx
+++ assets/dashboard/js/pages/components/Styleguide/Styleguide.jsx
@@ -5,10 +5,12 @@ import { StyleguideAlert }    from "./components/StyleguideAlert";
 import { StyleguideForm }     from "./components/StyleguideForm";
 import { StyleguideAside }    from "./components/StyleguideAside";
 import { StyleguideMaps }     from "./components/StyleguideMaps";
+import { StyleguideTable } from "./components/StyleguideTable";
 
 export class Styleguide extends Component {
     render () {
         return <div className="main-content">
+            <StyleguideTable />
             <StyleguideMaps />
             <StyleguideAside />
             <StyleguideForm />
