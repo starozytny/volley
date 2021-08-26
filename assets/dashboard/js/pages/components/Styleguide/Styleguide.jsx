8457484b5b22c0dc527d3349a1d1e7d7adf4aa6a
Shanbora <chanbora@logilink.fr>
2021-08-26 10:57:31 +0200

    accordion styleguide

--- assets/dashboard/js/pages/components/Styleguide/Styleguide.jsx
+++ assets/dashboard/js/pages/components/Styleguide/Styleguide.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
 
-import { StyleguideButton }   from "./components/StyleguideButton";
-import { StyleguideAlert }    from "./components/StyleguideAlert";
-import { StyleguideForm }     from "./components/StyleguideForm";
-import { StyleguideAside }    from "./components/StyleguideAside";
-import { StyleguideMaps }     from "./components/StyleguideMaps";
-import { StyleguideTable }    from "./components/StyleguideTable";
-import { StyleguidePlanning } from "./components/StyleguidePlanning";
+import { StyleguideButton }    from "./components/StyleguideButton";
+import { StyleguideAlert }     from "./components/StyleguideAlert";
+import { StyleguideForm }      from "./components/StyleguideForm";
+import { StyleguideAside }     from "./components/StyleguideAside";
+import { StyleguideMaps }      from "./components/StyleguideMaps";
+import { StyleguideTable }     from "./components/StyleguideTable";
+import { StyleguidePlanning }  from "./components/StyleguidePlanning";
+import { StyleguideAccordion } from "./components/StyleguideAccordion";
 
 export class Styleguide extends Component {
     render () {
         return <div className="main-content">
+            <StyleguideAccordion />
             <StyleguidePlanning />
             <StyleguideForm />
             <StyleguideTable />
